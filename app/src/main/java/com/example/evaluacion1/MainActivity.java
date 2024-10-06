f2e517bfa855aebdbf171f4a1fa72dd443e3ea53
TheLouisXD <106026658+TheLouisXD@users.noreply.github.com>
2024-10-06 01:21:05 -0300

    Añadida la funcionalidad de levantar el telefono para enviar reporte

--- app/src/main/java/com/example/evaluacion1/MainActivity.java
+++ app/src/main/java/com/example/evaluacion1/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.evaluacion1;
 
 import android.app.AlertDialog;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -14,14 +20,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    // Variables de los botones
 
     Button bEnviar, bSalir;
 
     EditText etNombre, etRut, etDescripcion;
 
-    boolean isAllFieldsCheked = false;
+    boolean Completado = false;
+
+    private SensorEvent event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +55,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                isAllFieldsCheked = CheckAllFields();
+                Completado = Validacion();
 
-                if (isAllFieldsCheked) {
-                    showConfirmationDialog();
+                if (Completado) {
+                    Confirmacion();
                 }
             }
         });
@@ -76,10 +87,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        super.onResume();
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if (sensors.size() > 0){
+            sm.registerListener(this, sensors.get(0), SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+
+        float y = event.values[1];
+
+        if (y > 9 && y < 20) {
+
+            Completado = Validacion();
+
+            if (Completado) {
+                Intent i = new Intent(MainActivity.this, Activity2.class);
+                startActivity(i);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
     }
 
 
-    private void showConfirmationDialog() {
+    private void Confirmacion() {
         new AlertDialog.Builder(this)
                 .setTitle("Confirmación")
                 .setMessage("¿Deseas enviar el reporte?")
@@ -92,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Validacion de inputs
-    private boolean CheckAllFields(){
+    private boolean Validacion(){
         if (etNombre.length() == 0) {
             etNombre.setError("Ingrese su nombre");
             return false;
